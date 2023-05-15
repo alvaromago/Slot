b1d3c5768b2ce12b1bb1680c78b83235862ffdcc
alvaromago <aalvaromago@gmail.com>
2023-05-15 13:27:38 +0200

    Primera clase

--- src/es/studium/Slot/Controlador.java
+++ src/es/studium/Slot/Controlador.java
@@ -1,6 +1,69 @@
 package es.studium.Slot;
 
-public class Controlador
-{
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+public class Controlador implements WindowListener, MouseListener
+{
+	Modelo modelo;
+	MenuPrincipal menuPrincipal;
+	
+	Controlador(Modelo m, MenuPrincipal mp)
+	{
+		this.modelo = m;
+		this.menuPrincipal = mp;
+		
+		this.menuPrincipal.addWindowListener(this);
+		this.menuPrincipal.addMouseListener(this);
+	}
+
+	public void windowOpened(WindowEvent e){}
+
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
+	}
+
+	public void windowClosed(WindowEvent e){}
+
+	public void windowIconified(WindowEvent e){}
+
+	public void windowDeiconified(WindowEvent e){}
+
+	public void windowActivated(WindowEvent e){}
+
+	public void windowDeactivated(WindowEvent e){}
+
+	public void mouseClicked(MouseEvent me)
+	{
+		int x = me.getX();
+		int y = me.getY();
+		
+		if(x>10&&x<50&&y>50&&y<90)
+		{
+			System.out.println("Nueva Partida");
+		}
+	}
+
+	public void mousePressed(MouseEvent me)
+	{
+		
+	}
+
+	public void mouseReleased(MouseEvent me)
+	{
+		
+	}
+
+	public void mouseEntered(MouseEvent me)
+	{
+		
+	}
+
+	public void mouseExited(MouseEvent me)
+	{
+		
+	}
 }
