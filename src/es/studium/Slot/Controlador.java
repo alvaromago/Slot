ac70c117aac46fcbd6b8aff8f522729a46f98d0b
alvaromago <aalvaromago@gmail.com>
2023-05-22 13:32:04 +0200

    Tercera clase

--- src/es/studium/Slot/Controlador.java
+++ src/es/studium/Slot/Controlador.java
@@ -1,11 +1,14 @@
 package es.studium.Slot;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.Connection;
 
-public class Controlador implements WindowListener, MouseListener
+public class Controlador implements ActionListener, WindowListener, MouseListener
 {
 	Modelo modelo;
 	MenuPrincipal menuPrincipal;
@@ -15,6 +18,7 @@ public class Controlador implements WindowListener, MouseListener
 	
 	int num1, num2, num3;
 	int dinero;
+	int tiradas;
 	
 	Controlador(Modelo m, MenuPrincipal mp)
 	{
@@ -25,6 +29,7 @@ public class Controlador implements WindowListener, MouseListener
 		this.menuPrincipal.addMouseListener(this);
 		
 		dinero = 5;
+		tiradas = 0;
 	}
 
 	public void windowOpened(WindowEvent e){}
@@ -38,6 +43,11 @@ public class Controlador implements WindowListener, MouseListener
 		else if(ranking!=null&&ranking.isActive())
 		{
 			ranking.setVisible(false);
+		}
+		else if(this.tablero.nombreJugador.isActive())
+		{
+			this.tablero.nombreJugador.setVisible(false);
+			tablero.setVisible(false);
 		}
 		else
 		{			
@@ -66,6 +76,8 @@ public class Controlador implements WindowListener, MouseListener
 			{
 				// Primera opción: Tablero
 				tablero = new Tablero();
+				dinero = 5;
+				tiradas = 0;
 				this.tablero.addWindowListener(this);
 				this.tablero.addMouseListener(this);
 			}
@@ -90,33 +102,76 @@ public class Controlador implements WindowListener, MouseListener
 		{
 			//Botón JUGAR de Tablero
 			if(x>20&&x<250&&y>210&&y<250)
-			{
+			{	
 				dinero--;
+				tiradas++;
 				num1 = modelo.aleatorio();
 				num2 = modelo.aleatorio();
 				num3 = modelo.aleatorio();
+				tablero.establecerPrimera(num1);
+				tablero.establecerSegunda(num2);
+				tablero.establecerTercera(num3);
 				// Comprobar premios
 				if(num1==num2&&num2==num3&&num1==14)
 				{
-					dinero = dinero + 50;
+					dinero = dinero + 25;
 				}
 				else if(num1==num2&&num2==num3&&num1>9&&num1<14)
 				{
-					dinero = dinero + 25;
+					dinero = dinero + 15;
 				}
 				else if(num1==num2&&num2==num3&&num1<=9)
 				{
-					dinero = dinero + 10;
+					dinero = dinero + 5;
 				}
-				if(dinero==0)
+				else if(num1==num2 || num1==num3 || num2 == num3)
 				{
-					System.out.println("Se acabó");
-					this.tablero.removeMouseListener(this);
+					dinero = dinero + 3;
 				}
-				else
-				{					
-					System.out.println("Tienes " + dinero + "€");
+			}
+			else if(x>20&&x<90&&y>140&&y<160)
+			{
+				num1++;
+				dinero--;
+				if(num1>14)
+				{
+					num1 = 1;
 				}
+				tablero.establecerPrimera(num1);
+			}
+			else if(x>100&&x<170&&y>140&&y<160)
+			{
+				num2++;
+				dinero--;
+				if(num2>14)
+				{
+					num2 = 1;
+				}
+				tablero.establecerSegunda(num2);
+			}
+			else if(x>180&&x<250&&y>140&&y<160)
+			{
+				num3++;
+				dinero--;
+				if(num3>14)
+				{
+					num3 = 1;
+				}
+				tablero.establecerTercera(num3);
+			}
+			if(dinero==0)
+			{
+				System.out.println("Se acabó");
+				System.out.println("Has realizado " + tiradas + " tiradas");
+				this.tablero.removeMouseListener(this);
+				this.tablero.nombreJugador.addWindowListener(this);
+				this.tablero.nombreJugador.addMouseListener(this);
+				this.tablero.btnAceptar.addActionListener(this);
+				this.tablero.nombreJugador.setVisible(true);
+			}
+			else
+			{					
+				System.out.println("Tienes " + dinero + "€");
 			}
 		}
 	}
@@ -128,4 +183,16 @@ public class Controlador implements WindowListener, MouseListener
 	public void mouseEntered(MouseEvent me){}
 
 	public void mouseExited(MouseEvent me){}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// Guardar el nombre en la BD
+		String nombre = this.tablero.txtNombre.getText();
+		Connection c = modelo.conectar();
+		modelo.insertar(c, nombre, tiradas);
+		// Cerrar el diálogo
+		this.tablero.nombreJugador.setVisible(false);
+		tablero.setVisible(false);
+	}
 }
--- src/es/studium/Slot/Modelo.java
+++ src/es/studium/Slot/Modelo.java
@@ -1,5 +1,7 @@
 package es.studium.Slot;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Random;
 
 public class Modelo
@@ -15,5 +17,35 @@ public class Modelo
 		Random aleatorio = new Random();
 		int numero = aleatorio.nextInt(14)+1;
 		return(numero);
+	}
+	// Conexión
+	public Connection conectar()
+	
+	{
+		Connection connection = null;
+		return connection;
+	}
+	// Desconexión
+	public void desconectar(Connection c)
+	{
+		try
+		{
+			c.close();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	// Consultar para el Ranking
+	public String ranking(Connection c)
+	{
+		String contenido = "";
+		String sentencia = "select * from ranking order by puntos desc limit 10";
+		return(contenido);
+	}
+	// Insertar en BD
+	public void insertar(Connection c, String nombre, int puntos)
+	{
+		String sentencia = "insert into ranking values (null, '" + nombre + "', " + puntos + ");";
 	}
 }
